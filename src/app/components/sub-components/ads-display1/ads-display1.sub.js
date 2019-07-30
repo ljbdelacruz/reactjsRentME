9bb064c3e0f664246319d8ec60fcba652c51f9ed
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-07-30 15:41:52 +0800

    updates notes and the ads-display for displaying ads for rent

---
+++ src/app/components/sub-components/ads-display1/ads-display1.sub.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './ads-display1.css'
+
+export class AdsDisplay1Sub extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            username:'',
+            password:''
+        }
+    }
+    render(){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
