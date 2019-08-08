5064267826edc164cde35e7eb6bb24cc9ba4c3b1
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-08-08 08:56:39 +0800

    setup view product page

---
+++ src/app/pages/viewproduct/viewproduct.page.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+
+class ViewProductPage extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={
+        }
+    }
+    render(){
+          return (
+            <div>
+                <p>View Product Info</p>
+            </div>
+          )
+    }
+}
+export default ViewProductPage
